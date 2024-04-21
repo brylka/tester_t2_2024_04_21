edf1710dc9e3b7b6154d20d8eed2055976ead41b
Bartosz Bryniarski <brylka@jelenia.com>
2024-04-21 13:48:55 +0200

    plik book.py

---
+++ book.py
@@ -0,0 +1,16 @@
+class Book:
+    def __init__(self, title, author, year):
+        self.title = title
+        self.author = author
+        self.year = year
+    def get_info(self):
+        pass
+    def change_title(self):
+        pass
+    def change_author(self):
+        pass
+    def change_year(self):
+        pass
+
+#book = Book("Wiedzmin", "Sapkowski", 1997)
+#print(book.year)
